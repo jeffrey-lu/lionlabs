5a777b6d95ac4fe60ef56eacc977218b6362d682
Jeffrey Lu <jeffrey.o.lu@gmail.com>
2013-10-20 17:54:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lionlabs::Application.config.secret_key_base = '738bdef1c12e4fbc0d4670fbe6369b3bf9b1dc7179fb70e99f34c19794ff7846ea8a09a366002fadddcb2a09acbfc14bd94fb6be23a5dc5262ce374e697334f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lionlabs::Application.config.session_store :cookie_store, key: '_lionlabs_session'
